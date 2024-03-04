66bd27ba764377d46bea725b8518f1181513572a
Garrett Smith <GSmith6719@gmail.com>
2024-03-04 16:03:28 -0600

    almost there

--- scripts/database.js
+++ scripts/database.js
@@ -8,61 +8,97 @@ const database = {
     fish: [
         {
             id: 1,
-            image: "flubble.jpg",
-            species: "Flubble Fish",
-            length: "6 inches",
-            name: "Bubble",
-            harvestLocation: "Underwater Candyland",
-            food: "Rainbow sprinkles"
+            imageUrl: "assets/Angelfish.png",
+            species: "Angelfish",
+            length: 8,
+            name: "Bubbles",
+            harvestLocation: "Caribbean Sea",
+            food: "Plankton"
         },
         {
             id: 2,
-            image: "bloopster.jpg",
-            species: "Bloopster Bass",
-            length: "8 inches",
-            name: "Bobby",
-            harvestLocation: "Bubbly Bay",
-            food: "Bubblegum balls"
+            imageUrl: "assets/Clownfish.png",
+            species: "Clownfish",
+            length: 4,
+            name: "Marlin",
+            harvestLocation: "Great Barrier Reef",
+            food: "Algae"
         },
         {
             id: 3,
-            image: "glitterfin.jpg",
-            species: "Glitterfin Guppy",
-            length: "4 inches",
-            name: "Glitzy",
-            harvestLocation: "Sparkle Springs",
-            food: "Fairy dust flakes"
+            imageUrl: "assets/Guppy.png",
+            species: "Guppy",
+            length: 2,
+            name: "Finn",
+            harvestLocation: "Amazon River",
+            food: "Fish flakes"
         },
         {
             id: 4,
-            image: "twinkletooth.jpg",
-            species: "Twinkletooth Tetra",
-            length: "3 inches",
-            name: "Twinkie",
-            harvestLocation: "Starry Stream",
-            food: "Pixie potion pellets"
+            imageUrl: "assets/Tuna.png",
+            species: "Tuna",
+            length: 60,
+            name: "Sushi",
+            harvestLocation: "Atlantic Ocean",
+            food: "Squid"
         },
         {
             id: 5,
-            image: "sparklescale.jpg",
-            species: "Sparklescale Swordtail",
-            length: "5 inches",
-            name: "Sparky",
-            harvestLocation: "Shimmering Shore",
-            food: "Unicorn tears"
+            imageUrl: "assets/Trout.png",
+            species: "Trout",
+            length: 18,
+            name: "Splash",
+            harvestLocation: "Freshwater Lake",
+            food: "Insects"
         },
         {
             id: 6,
-            image: "whiskerwhirl.jpg",
-            species: "Whiskerwhirl Wrasse",
-            length: "7 inches",
-            name: "Whirly",
-            harvestLocation: "Whimsical Waters",
-            food: "Magic marshmallows"
+            imageUrl: "assets/Swordfish.png",
+            species: "Swordfish",
+            length: 100,
+            name: "Blade",
+            harvestLocation: "Pacific Ocean",
+            food: "Fish"
+        }
+    ],
+    locations: [
+        {
+            id: 1,
+            imageUrl:"",
+            location: "Caribbean Sea"
+        },
+        {
+            id: 2,
+            imageUrl:"",
+            location: "Great Barrier Reef"
+        },
+        {
+            id: 3,
+            imageUrl:"",
+            location: "Amazon River"
+        },
+        {
+            id: 4,
+            imageUrl:"",
+            location: "Atlantic Ocean"
+        },
+        {
+            id: 5,
+            imageUrl:"",
+            location: "Freshwater Lake"
+        },
+        {
+            id: 6,
+            imageUrl:"",
+            location: "Pacific Ocean"
         }
     ]
 }
 
 export const getFish = () => {
     return database.fish.map(fish => ({...fish}))
+}
+
+export const getlocations = () => {
+    return database.locations.map(locations => ({...locations}))
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,23 @@
-import { getFish } from './database.js'
+import { fishList } from './fish.js'
 
-const allFish = getFish()
 
+const allFish = fishList()
 for (const fish of allFish) {
     console.log(fish)
 }
+
+
+
+
+/*
+    What is the CSS selector for the element where you
+    want to display the fish?
+
+    Use . for elements with a "class" attribute
+    Use # for elements with an "id" attribute
+ */
+
+const parentHTMLElement = document.querySelector(".fish-container")
+
+
+parentHTMLElement.innerHTML = fishList()
